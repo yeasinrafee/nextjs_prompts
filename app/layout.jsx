f7cc51ac0874faaa7e26c076d452fc3c829a9f0b
yeasinrafee <mdyeasinrafi7@gmail.com>
2024-08-15 00:02:52 +0600

    Initialization

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import '@styles/globals.css';
+
+export const matadata = {
+  title: 'Promptobia',
+  description: 'Discover & Share AI Prompts',
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang='en'>
+      <body>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
+
+        <main className='app'>{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
